ad1cbcbc1c7f758e07c7f8a35a183994dae51d33
shinogamii <davidreinhart98@gmail.com>
2017-04-05 20:30:12 +0000

    sample

--- test_pubnub_SDK4Python.py
+++ test_pubnub_SDK4Python.py
@@ -3,8 +3,8 @@ from pubnub.pubnub import PubNub
 import sys
 
 mypubnubconfig = PNConfiguration()
-mypubnubconfig.subscribe_key = 'sub-c-231884a6-14a6-11e7-bc52-02ee2ddab7fe'
-mypubnubconfig.publish_key = 'pub-c-898d4c55-2abe-4317-b23c-c20ec9f09d19'
+mypubnubconfig.subscribe_key = 'your subkey'
+mypubnubconfig.publish_key = 'your pubkey'
 pubnub = PubNub(mypubnubconfig)
 
 
